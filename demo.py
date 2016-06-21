3e5ae914c3fb375c571eca442202b0b9dcabb6a8
djatha15 <datha@purdue.edu>
2016-06-20 21:54:17 -0400

    updated for csv

--- demo.py
+++ demo.py
@@ -18,7 +18,7 @@ from fast_rcnn.config import cfg
 from fast_rcnn.test import im_detect
 from fast_rcnn.nms_wrapper import nms
 from utils.timer import Timer
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import numpy as np
 import scipy.io as sio
 import caffe, os, sys, cv2
@@ -38,39 +38,49 @@ NETS = {'vgg16': ('VGG16',
                   'ZF_faster_rcnn_final.caffemodel')}
 
 
-def vis_detections(im, class_name, dets, thresh=0.5):
+def vis_detections(im, class_name, dets, output_file, image_name, thresh=0.5):
     """Draw detected bounding boxes."""
     inds = np.where(dets[:, -1] >= thresh)[0]
     if len(inds) == 0:
         return
 
     im = im[:, :, (2, 1, 0)]
-    fig, ax = plt.subplots(figsize=(12, 12))
-    ax.imshow(im, aspect='equal')
+    #fig, ax = plt.subplots(figsize=(12, 12))
+    #ax.imshow(im, aspect='equal')
+
     for i in inds:
         bbox = dets[i, :4]
         score = dets[i, -1]
 
-        ax.add_patch(
-            plt.Rectangle((bbox[0], bbox[1]),
-                          bbox[2] - bbox[0],
-                          bbox[3] - bbox[1], fill=False,
-                          edgecolor='red', linewidth=3.5)
-            )
-        ax.text(bbox[0], bbox[1] - 2,
-                '{:s} {:.3f}'.format(class_name, score),
-                bbox=dict(facecolor='blue', alpha=0.5),
-                fontsize=14, color='white')
+        #deegan added, writing to file to output data
+        #file name, x1, y1, x2, y2, class, score
+        img_name_out = image_name.split('/')
+        length = len(img_name_out)
+        output_file.write(img_name_out[length-1] + ',')
+        output_file.write(str(bbox[0]) + ',' + str(bbox[1]) + ',' + str(bbox[2]) + ',' + str(bbox[3]) + ',')
+        output_file.write(class_name + ',' + str(score) + '\n')
 
-    ax.set_title(('{} detections with '
-                  'p({} | box) >= {:.1f}').format(class_name, class_name,
-                                                  thresh),
-                  fontsize=14)
-    plt.axis('off')
-    plt.tight_layout()
-    plt.draw()
+        #ax.add_patch(
+        #    plt.Rectangle((bbox[0], bbox[1]),
+        #                  bbox[2] - bbox[0],
+        #                  bbox[3] - bbox[1], fill=False,
+        #                  edgecolor='red', linewidth=3.5)
+        #    )
+        #ax.text(bbox[0], bbox[1] - 2,
+        #        '{:s} {:.3f}'.format(class_name, score),
+        #        bbox=dict(facecolor='blue', alpha=0.5),
+        #        fontsize=14, color='white')
 
-def demo(net, image_name):
+    #ax.set_title(('{} detections with '
+    #              'p({} | box) >= {:.1f}').format(class_name, class_name,
+    #                                              thresh),
+    #              fontsize=14)
+    #plt.axis('off')
+    #plt.tight_layout()
+    #plt.draw()
+
+
+def demo(net, image_name, output_file):
     """Detect object classes in an image using pre-computed object proposals."""
 
     # Load the demo image
@@ -81,12 +91,13 @@ def demo(net, image_name):
     timer = Timer()
     timer.tic()
     scores, boxes = im_detect(net, im)
+    
     timer.toc()
     print ('Detection took {:.3f}s for '
            '{:d} object proposals').format(timer.total_time, boxes.shape[0])
 
     # Visualize detections for each class
-    CONF_THRESH = .3  #0.8 was the default
+    CONF_THRESH = .5  #0.8 was the default
     NMS_THRESH = 0.3
     for cls_ind, cls in enumerate(CLASSES[1:]):
         cls_ind += 1 # because we skipped background
@@ -96,7 +107,8 @@ def demo(net, image_name):
                           cls_scores[:, np.newaxis])).astype(np.float32)
         keep = nms(dets, NMS_THRESH)
         dets = dets[keep, :]
-        vis_detections(im, cls, dets, thresh=CONF_THRESH)
+
+        vis_detections(im, cls, dets, output_file, image_name, thresh=CONF_THRESH)
 
 def parse_args():
     """Parse input arguments."""
@@ -108,6 +120,10 @@ def parse_args():
                         action='store_true')
     parser.add_argument('--net', dest='demo_net', help='Network to use [vgg16]',
                         choices=NETS.keys(), default='vgg16')
+    #deegan added
+    parser.add_argument('--output_file', dest='out_file_name', 
+                        help='File to ouput bounding box and class info',
+                        default='output_file.csv')
 
     args = parser.parse_args()
 
@@ -142,13 +158,20 @@ if __name__ == '__main__':
         _, _= im_detect(net, im)
 
 	# deegan insert
-	path = os.path.join(cfg.DATA_DIR, 'demo') + '/*.jpg'
+	#path = os.path.join(cfg.DATA_DIR, 'demo') + '/*.jpg'
+	path = os.path.join(cfg.DATA_DIR, 'demo') + '/*.*'
+	#path = os.path.join(cfg.DATA_DIR, 'demo') + '/*.bmp'
+	#path = os.path.join(cfg.DATA_DIR, 'demo') + '/*.png'
     im_names = glob.glob(path)
     print im_names
+
+    output_file = open(args.out_file_name, 'w') ##needs to be an argument
+    output_file.write("Filename, x1,y1,x2,y2,Class,Score\n")
 
     for im_name in im_names:
         print '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
         print 'Demo for data/demo/{}'.format(im_name)
-        demo(net, im_name)
+        demo(net, im_name, output_file)
 
-    plt.show()
+    output_file.close()
+    #plt.show()
